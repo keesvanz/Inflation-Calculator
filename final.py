5bb259a983848c7691c0d2c8d1f3c59ac4d6fb9e
keesvanz <kvanderaa@gmail.com>
2017-03-17 09:33:59 -0500

    [Google friday]

---
+++ final.py
@@ -0,0 +1,18 @@
+print("Python Inflation Calculator (PIC)")
+
+raw_input("$")
+raw_input("in >")
+print("has the same buying power as,")
+print("in 2017")
+
+
+CPI2016 = 239.5
+
+CPI1913 = 9.9
+CPI1914 = 10.0
+CPI1915 = 10.1
+CPI1916 = 10.9
+
+
+for inflation in year:
+	CPI2016/CPI2*HUSD=USD2016
